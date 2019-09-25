0f27efdcecec4f7012bd409822ad921d4fe0d2e0
Magesh Kuppan <tkmagesh77@gmail.com>
2019-09-25 10:42:47 +0800

    calculator-2 solution - complete

--- first-app/src/app/calculator/calculator2.component.ts
+++ first-app/src/app/calculator/calculator2.component.ts
@@ -6,28 +6,5 @@ import { CalculatorModel } from './calculatorModel';
     templateUrl : 'calculator2.component.html'
 })
 export class Calculator2Component{
-
     model : CalculatorModel = new CalculatorModel();
-
-    operator : string = 'add';
-
-    onCalculateClick(){
-        switch (this.operator) {
-            case 'add':
-                this.model.add();
-                break;
-            case 'subtract':
-                this.model.subtract();
-                break;
-            case 'multiply':
-                this.model.multiply();
-                break;
-            case 'divide':
-                this.model.divide();
-                break;
-            default:
-                break;
-        }
-    }
-
 }
