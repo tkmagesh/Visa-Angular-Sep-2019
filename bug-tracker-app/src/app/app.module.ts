de85a215e78018d0d4886eb5eba1b91d95e039c2
Magesh Kuppan <tkmagesh77@gmail.com>
2019-09-26 15:58:01 +0800

    Day-03 complete

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { UtilsModule } from './utils/utils.module';
+
 import { AppComponent } from './app.component';
 import { BugTrackerComponent } from './bug-tracker/bug-tracker.component';
 import { BugStatsComponent } from './bug-tracker/views/bugStats.component';
@@ -9,11 +11,9 @@ import { BugEditComponent } from './bug-tracker/views/bugEdit.component';
 import { BugItemComponent } from './bug-tracker/views/bug-item.component';
 
 import { BugOperationsService } from './bug-tracker/services/bugOperations.service';
-import { TrimTextPipe } from './bug-tracker/pipes/trimText.pipe';
-import { SortPipe } from './bug-tracker/pipes/sort.pipe';
 import { ClosedCountPipe } from './bug-tracker/pipes/closedCount.pipe';
 import { BugStorageService } from './bug-tracker/services/bugStorage.service';
-import { ElapsedPipe } from './bug-tracker/pipes/elapsed.pipe';
+
 
 @NgModule({
   declarations: [
@@ -22,14 +22,12 @@ import { ElapsedPipe } from './bug-tracker/pipes/elapsed.pipe';
     BugStatsComponent,
     BugEditComponent,
     BugItemComponent,
-    ClosedCountPipe,
-    TrimTextPipe,
-    SortPipe,
-    ElapsedPipe
+    ClosedCountPipe
   ],
   imports: [
     BrowserModule
     , FormsModule
+    , UtilsModule
   ],
   providers: [
     BugOperationsService,
